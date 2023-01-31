c05c74ceb2b51c751972c9d64abb15c6e0657c8a
Kyle Harrison <redactedprofile@gmail.com>
2023-01-30 20:17:31 -0800

    Starting Day 3

---
+++ Day3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+int main()
+{
+	std::cout << "Advent of Code, Day 3: Rucksack Reorganization." << std::endl;
+	std::cout << "========================================" << std::endl;
+
+	std::cout << "Loading input.." << std::endl;
+
+	std::ifstream file("input.txt");
+
+	if (file.is_open())
+	{
+		std::string line;
+		while (std::getline(file, line)) {
+	
+		}
+	}
+
+	file.close();
+
+	std::cout << "Part 1 Total Score: " << "" << std::endl;
+	
+	return 0;
+}
