948d0ed214da366c4402aeafa68d74c7925dc66f
Kyle Harrison <redactedprofile@gmail.com>
2023-02-01 00:13:42 -0800

    Day 4 part 1: removed stringBuilder lineScanner approach to stream extractor approach. Brings it all down to like, one line

--- Day4/main.cpp
+++ Day4/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <algorithm>
@@ -24,49 +26,11 @@ int main()
 		while (std::getline(file, line)) {
 			std::vector< std::pair<uint32_t, uint32_t> > group(2);
 
-			uint16_t 
-				groupIndex = 0, 
-				campIdSlot = 0;
+			std::istringstream stream(line);
+			char del, sep;
 
-			// Format of each line is like: "24-91,80-92"
-			std::string activeStringBuilder = "";
-
-			auto compileStringFunction = [&groupIndex, &campIdSlot, &group](std::string &stringBuilder) {
-				uint32_t campId = std::stoi(stringBuilder);
-
-				if (campIdSlot == 0)
-					group[groupIndex].first = campId;
-				else
-					group[groupIndex].second = campId;
-
-				stringBuilder = "";
-			};
-
-			for (size_t i = 0; i < line.length(); ++i)
-			{
-				std::string c = std::string{ line[i] };
-				
-				if (c == "-") {
-					compileStringFunction(activeStringBuilder);
-
-					campIdSlot++;
-					continue;
-				}
-				else if (c == ",") {
-					compileStringFunction(activeStringBuilder);
-
-					groupIndex++;
-					campIdSlot = 0; // reset id slot, new instance
-					continue;
-				}
-
-				activeStringBuilder.append(c);
-
-				// one last time for last iteration
-				if ((i + 1) == line.length()) {
-					compileStringFunction(activeStringBuilder);
-				}
-			}
+			// parse the string using the stream extractor
+			stream >> group[0].first >> del >> group[0].second >> sep >> group[1].first >> del >> group[1].second;
 
 			// for part 1 we want to collect all instances of where one elf's range entirely engulfs the other elfs duties
 			if ( group[0].first <= group[1].first && group[0].second >= group[1].second ||
@@ -74,7 +38,6 @@ int main()
 			{
 				fullyContainsCount++;
 			}
-
 		}
 	}
 
