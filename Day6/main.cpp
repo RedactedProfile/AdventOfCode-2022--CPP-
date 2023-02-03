35e21f02591fdeeeeaed9e12892763743f8b8651
Kyle Harrison <redactedprofile@gmail.com>
2023-02-03 12:58:31 -0800

    Day 6, Part 1: complete

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-#include <variant>
+#include <map>
 #include <ExecutionTime.h>
 
 int main()
@@ -28,14 +28,30 @@ int main()
 		std::string line;
 		while (std::getline(file, line)) {
 
-			std::cout << line << std::endl;
+			//std::cout << line << std::endl;
 
+			for (uint16_t i = 3; i < line.size(); ++i)
+			{
+				std::string sub = line.substr(i - 3, 4);
+				std::map<std::string, bool> storage = {};
+				storage[std::string{ sub[0] }] = true;
+				storage[std::string{ sub[1] }] = true;
+				storage[std::string{ sub[2] }] = true;
+				storage[std::string{ sub[3] }] = true;
+
+				if (storage.size() == 4)
+				{
+					std::cout << "Part 1: Unique 4 found at itr " << i + 1 << ":" << sub << "." << std::endl;
+					break;
+
+				}
+			}
 		}
 	}
 
 	file.close();
 
-	std::cout << "Part 1: : " << std::endl;
+	//std::cout << "Part 1: : " << std::endl;
 	std::cout << "Part 2: : " << std::endl;
 
 	timer.stop();
