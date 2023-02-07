4e6ba31819e0c1079d48b200aca86813034a9538
Kyle Harrison <redactedprofile@gmail.com>
2023-02-06 16:29:04 -0800

    map renderer

--- Day9/main.cpp
+++ Day9/main.cpp
@@ -6,13 +6,60 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 #include <ExecutionTime.h>
+
+struct Vector2i
+{
+	int x = 0;
+	int y = 0;
+};
+
+class Entity
+{
+public:
+	const char* label = "-";
+	Vector2i coordinates = { 0, 0 };
+	int priority = 0;
+};
+
+class eInfoStart : public Entity
+{
+public:
+	eInfoStart() : Entity()
+	{
+		label = "s";
+	}
+};
+
+class eHead : public Entity
+{
+public:
+	eHead() : Entity()
+	{
+		label = "H";
+		priority = 2;
+	}
+};
+
+class eTail : public Entity
+{
+public:
+	eTail() : Entity()
+	{
+		label = "T";
+		priority = 1;
+	}
+};
 
 class Map
 {
 public:
 	const char* map[MAP_WIDTH][MAP_HEIGHT] = {};
 
+	std::unordered_map<std::string, Entity*> entities = {};
+
+	
 	Map()
 	{
 		initialize();
@@ -23,6 +70,15 @@ public:
 		for (int y = 0; y < MAP_HEIGHT; ++y)
 			for (int x = 0; x < MAP_WIDTH; ++x)
 				map[y][x] = ".";
+
+		entities["info_start"] = new eInfoStart();
+		entities["head"] = new eHead();
+		entities["tail"] = new eTail();
+
+		Vector2i center = { MAP_WIDTH / 2, MAP_HEIGHT / 2 };
+		entities["info_start"]->coordinates = center;
+		entities["head"]->coordinates = center;
+		entities["tail"]->coordinates = center;
 	}
 
 	void Render()
@@ -32,7 +88,31 @@ public:
 			std::string line = "";
 			for (int x = 0; x < MAP_WIDTH; ++x)
 			{
-				line.append(std::string{ map[y][x] });
+				std::string ch = std::string{ map[y][x] };
+				
+				std::vector<Entity*> entitiesHere = {};
+
+				for (auto& it : entities)
+				{
+					Vector2i eCoord = it.second->coordinates;
+					if (eCoord.x == x && eCoord.y == y)
+					{
+						//ch = it.second->label;
+						entitiesHere.push_back(it.second);
+					}
+				}
+
+				if (entitiesHere.size() == 1)
+				{
+					ch = entitiesHere[0]->label;
+				}
+				else if (entitiesHere.size() > 0)
+				{
+					std::sort(entitiesHere.begin(), entitiesHere.end(), [](Entity* a, Entity* b) { return a->priority > b->priority; });
+					ch = entitiesHere[0]->label;
+				}
+
+				line.append(ch);
 			}
 			std::cout << line << std::endl;
 		}			
